78b265340882a1a444151df30b0a936c8c31c0ee
HanSangWook <qazws78941@gmail.com>
2023-05-18 20:04:35 +0900

    UI 완성

---
+++ lib/src/components/input_field.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String hintText;
+
+  const InputField({
+    super.key,
+    this.controller,
+    required this.hintText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.blue, width: 2.0),
+            borderRadius: BorderRadius.circular(15.0)),
+        enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.black, width: 2.0),
+            borderRadius: BorderRadius.circular(15.0)),
+        hintText: hintText,
+      ),
+    );
+  }
+}
--- lib/src/view/app.dart
+++ lib/src/view/app.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_calculator/src/components/input_field.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter GetX Calculator'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _input(),
+              _button(),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: _result(),
+    );
+  }
+
+  Widget _result() {
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text(
+          '결과 : Result',
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+    );
+  }
+
+  Widget _input() {
+    return Column(
+      children: const [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: InputField(
+            hintText: '첫번째 숫자',
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: InputField(
+            hintText: '두번째 숫자',
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _button() {
+    return ElevatedButton(onPressed: () {}, child: const Text('더하기'));
   }
 }
